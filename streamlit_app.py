45b3d587310f014ceb1306380890244e4dfe9a30
once upon a gamer <159383852+onceuponagamer@users.noreply.github.com>
2024-05-23 18:18:30 +0000

    Changes to be committed: 	new file:   output_2024-05-23.mp4 	modified:   streamlit_app.py 	modified:   util/prediction.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,9 @@ import cv2
 import io
 from util.prediction import video_predict
 
-#st.title('hello world!')
-
 temp_file_to_save = './temp_file_1.mp4'
 temp_file_result  = './temp_file_2.mp4'
+output_file = './output_2024-05-23.mp4'
 
 # func to save BytesIO on a drive
 def write_bytesio_to_file(filename, bytesio):
@@ -28,6 +27,10 @@ def main():
         This project developed by %s
         """
         % _author_)
+    
+    with open(temp_file_to_save, "rb") as video_file:
+        video_bytes = video_file.read()
+        st.video(video_bytes)
 
     tab1, tab2, tab3 = st.tabs(["Upload a File", "Use Webcam", "..."])
     user_input = None
@@ -39,12 +42,8 @@ def main():
     user_input = None
 
     if generate_file_input and uploaded_file is not None:
-        #file_type = uploaded_file.type
-        #st.write(file_type)
         user_input = io.BytesIO(uploaded_file.read())
-        #st.write("filename:", uploaded_file.name)
-        #st.write(user_input)
-        # save uploaded video to disc
+        # save uploaded video to disk
         write_bytesio_to_file(temp_file_to_save, user_input)
 
     #if user_input:
@@ -55,9 +54,12 @@ def main():
         if user_input:
             with st.spinner("Generating Video..."):
                 try:
-                    pass
                     #st.video(user_input)
-                    #video_predict(temp_file_to_save)
+                    video_path = video_predict(temp_file_to_save)
+                    st.write(video_path)
+                    video_file = open(video_path, 'rb')
+                    video_bytes = video_file.read()
+                    st.video(video_bytes)
                 except Exception as e:
                     st.error(f"An error occurred: {e}")
 
--- util/prediction.py
+++ util/prediction.py
@@ -1,18 +1,52 @@
 import cv2
+import datetime
 
 face_cascade = cv2.CascadeClassifier('files/haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('files/haarcascade_eye.xml')
 
 def video_predict(file):
+    # Capture
     cap = cv2.VideoCapture(file)
+
+    # Recording
+    frame_width = int(cap.get(3))
+    frame_height = int(cap.get(4))
+    output_video_path = f'output_{datetime.date.today()}.mp4' 
+    out = cv2.VideoWriter(output_video_path, cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), 10, (frame_width, frame_height))
+
+    scale_factor = 1.1
+    min_neighbours_for_faces = 5
+    min_neighbours_for_eyes = 5
+    min_size_w = 240
+    min_size_h = 240
+    min_size_w_eye = 60
+    min_size_h_eye = 60
+
+    # Stream data
+    total_frames = 0
 
     while True:
         ret, img = cap.read()
         if ret == False:
             break
+        
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray,scaleFactor=scale_factor,minNeighbors=min_neighbours_for_faces,minSize=(min_size_w,min_size_h),flags=cv2.CASCADE_SCALE_IMAGE)
 
-        cv2.imshow('Distraction Detection', img)
+        for (x,y,w,h) in faces:
+            cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+            # Frames
+            total_frames += 1
+            cv2.putText(img, f"Frames: {total_frames}", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+
+            out.write(img)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
     cap.release()
+    out.release()
     cv2.destroyAllWindows()
     cv2.waitKey(1)
+
+    return output_video_path
